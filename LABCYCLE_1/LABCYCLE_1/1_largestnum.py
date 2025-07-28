b7ee4e64fc985b658c112fa591bb4a9952ac4ec0
ayishahudakt <24mcag01@meaec.edu.in>
2025-07-28 14:34:15 +0530

    First commit

--- LABCYCLE_1/LABCYCLE_1/1_largestnum.py
+++
@@ -1,3 +0,0 @@
-n=list(map(int, input("enter numbers by space:").split()))
-large=max(n)
-print(large)
--- LABCYCLE_1/LABCYCLE_1/2_remDuplicate.py
+++
@@ -1,5 +0,0 @@
-n=list(map(int,input("enter number with duplicates seperated by space:").split()))
-print(n)
-unique=list(set(n))
-print("without duplicates:",unique)
-
